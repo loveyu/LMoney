722afc5abf37ecf9e7b8be5ca3ee49f369970a8a
loveyu <admin@loveyu.info>
2012-07-02 21:34:46 +0800

    Fix syntax error of 'a,li'

--- application/views/user/template/menu.php
+++ application/views/user/template/menu.php
@@ -1,8 +1,12 @@
+<!-- 菜单文件开始 -->
 <div id="menu">
 <ul>
-<a href="<?=site_url("user/edit_info")?>"><li>修改信息</li></a>
-<a href="<?=site_url("user/safe")?>"><li>安全设置</li></a>
-<a href="<?=site_url("user/password")?>"><li>密码修改</li></a>
-<a href="<?=site_url("user/login_history")?>"><li>登录查询</li></a>
+<li<?=get_menu_active('index')?>><a href="<?=site_url("user")?>">个人中心</a></li>
+<li<?=get_menu_active('edit')?>><a href="<?=site_url("user/edit_info")?>">修改信息</a></li>
+<li<?=get_menu_active('safe')?>><a href="<?=site_url("user/safe")?>">安全设置</a></li>
+<li<?=get_menu_active('password')?>><a href="<?=site_url("user/password")?>">密码修改</a></li>
+<li<?=get_menu_active('login')?>><a href="<?=site_url("user/login_history")?>">登录查询</a></li>
 </ul>
 </div>
+<!-- 菜单文件结束 -->
+
