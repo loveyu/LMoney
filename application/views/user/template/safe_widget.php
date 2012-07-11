35c5981a11a2a2bd5abe825adc6b0c75a60f1e53
loveyu <admin@loveyu.info>
2012-07-11 15:44:34 +0800

    add edit email menu

--- application/views/user/template/safe_widget.php
+++ application/views/user/template/safe_widget.php
@@ -6,6 +6,7 @@
 <li<?=get_menu_active('active',1)?>><a href="<?=site_url('user/active')?>">账户激活</a></li>
 <li<?=get_menu_active('PIN',1)?>><a href="<?=site_url('user/PIN')?>">修改安全码</a></li>
 <li<?=get_menu_active('password',1)?>><a href="<?=site_url('user/password')?>">修改账号密码</a></li>
+<li<?=get_menu_active('email',1)?>><a href="<?=site_url('user/email')?>">修改邮箱账号</a></li>
 <li<?=get_menu_active('HPIN',1)?>><a href="<?=site_url("user/HPIN")?>">修改登录历史密码</a></li>
 </ul>
 
