86e96f7902eda320fc0e52e81e715fecd068bfb9
loveyu <admin@loveyu.info>
2012-07-11 15:44:02 +0800

    add edit email page

---
+++ application/views/user/edit_email.php
@@ -0,0 +1,13 @@
+
+<div id="safe">
+<?php $this->load->view('user/template/safe_widget');?>
+<div class="safe-page">
+
+<div id="edit_email">
+
+</div>
+
+</div><!-- page -->
+
+</div><!-- safe -->
+<div class="clear"></div>
